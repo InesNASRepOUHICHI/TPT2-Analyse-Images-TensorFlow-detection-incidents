880565d8ec4dfcc4cc3d7f8a18506243b91c92dc
Ines NASR <ines.nasr.93@gmail.com>
2019-02-09 23:04:42 +0100

    Activer CORS sur le projet NodeJS

--- Api-Registration-Node-Mongo/server.js
+++ Api-Registration-Node-Mongo/server.js
@@ -9,6 +9,11 @@ const errorHandler = require('_helpers/error-handler');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
 
 // use JWT auth to secure the api
 app.use(jwt());
--- FrontEnd/src/app/_services/user.service.ts
+++ FrontEnd/src/app/_services/user.service.ts
@@ -1,7 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { environment } from '@environments/environment';
 import { User } from '@app/_models';
 
 @Injectable({ providedIn: 'root' })
@@ -9,22 +7,22 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+        return this.http.get<User[]>(`http://localhost:4000/users/users`);
     }
 
     getById(id: number) {
-        return this.http.get(`${environment.apiUrl}/users/${id}`);
+        return this.http.get(`http://localhost:4000/users/${id}`);
     }
 
     register(user: User) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
+        return this.http.post(`http://localhost:4000/users/register`, user);
     }
 
     update(user: User) {
-        return this.http.put(`${environment.apiUrl}/users/${user.id}`, user);
+        return this.http.put(`http://localhost:4000/users/${user.id}`, user);
     }
 
     delete(id: number) {
-        return this.http.delete(`${environment.apiUrl}/users/${id}`);
+        return this.http.delete(`http://localhost:4000/users/${id}`);
     }
 }
